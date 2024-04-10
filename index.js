b805d8d77ae01097c0f96d1cd1afcd41c2dd3b6d
pamela2002 <sroychoudhury65@gmail.com>
2024-04-11 01:25:37 +0530

    Basic implementation of socket.io

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+const exp = require('constants');
 const express = require('express');
-const app = express();
+const { Server } = require("socket.io");
 const http = require('http');
+
+
+const app = express();
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+const io = new Server(server);
+io.on('connection', (socket) => {
+    // console.log('A  New user has connected ', socket.id);
+    socket.on("user-message", (message) => {
+        // console.log("A new User Message",message);
+        io.emit("message", message);
+    })
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    return res.sendFile('./public/index.html');
+})
+
+server.listen(9000, () => {
+    console.log('Server started at PORT:9000');
 });
